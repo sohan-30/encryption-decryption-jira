0c9151a480bd9d5d4973c49c19b61adc90ae3cc4
sohan2704 <sohan2704@users.noreply.github.com>
2025-08-31 16:07:39 +0530

    AA-10 Initialize repo with skeleton, README, .gitignore

---
+++ README.md
@@ -0,0 +1,7 @@
+# Encryption–Decryption (Jira-tracked)
+
+Python program that:
+- Encrypts by taking even-indexed chars, then odd-indexed chars.
+- Decrypts by reconstructing original text.
+
+Jira project key: **AA** (e.g., AA-6, AA-7, ...).
---
+++ encrypt_decrypt.py
@@ -0,0 +1,8 @@
+def encrypt(text: str) -> str:
+    raise NotImplementedError("TODO in AA-6")
+
+def decrypt(cipher: str) -> str:
+    raise NotImplementedError("TODO in AA-7 / AA-24")
+
+if __name__ == "__main__":
+    print("CLI will be added in AA-8")
