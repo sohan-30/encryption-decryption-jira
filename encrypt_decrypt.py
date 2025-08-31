6f0d09021984b2de31b6b2fe3adccf90fe54e610
sohan2704 <sohan2704@users.noreply.github.com>
2025-08-31 18:28:34 +0530

    AA-7 Add initial decrypt() implementation (buggy for odd lengths)

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -4,7 +4,18 @@ def encrypt(text: str) -> str:
     return even + odd
 
 def decrypt(cipher: str) -> str:
-    raise NotImplementedError("TODO in AA-7 / AA-24")
+    # BUGGY: fails for odd-length strings
+    half = len(cipher) // 2
+    even = cipher[:half]
+    odd = cipher[half:]
+    out = []
+    for i in range(len(cipher)):
+        if i % 2 == 0:
+            out.append(even[i // 2])
+        else:
+            out.append(odd[i // 2])
+    return ''.join(out)
 
 if __name__ == "__main__":
     print("CLI will be added in AA-8")
+    print(encrypt("message")) 
